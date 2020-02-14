edbd23075e848358c360b828754ed8655129bbe4
Jonathan S. Fisher <exabrial@gmail.com>
2020-02-14 11:31:29 -0600

    add proposed interfaces/annotations

---
+++ api/src/main/java/jakarta/jms/OutboundMessageBodyConverter.java
@@ -0,0 +1,16 @@
+package jakarta.jms;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OutboundMessageBodyConverter {
+  Class<? extends MessageBodyConverter<? extends Message>> messageBodyConverter();
+}
